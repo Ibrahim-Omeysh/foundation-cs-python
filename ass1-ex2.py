0091155162d22c15c5ad457f4c877473395d047b
Ibrahim Omeysh <148053100+Ibrahim-Omeysh@users.noreply.github.com>
2023-10-20 20:41:17 +0300

    assignment_01_Ibrahim_omeysh_[fcs49]

---
+++ ass1-ex2.py
@@ -0,0 +1,17 @@
+def main():
+    n=input('please insert a number > 0: ')
+    while (not n.isnumeric()):
+        n=input('please insert a valid input: ')
+    def div(x):
+        x=int(x)
+        d=[]
+        str='all the number can divide 0 be smart :P'
+        if x==1: return 1
+        elif x==0: return str
+        else:
+            for i in range(1,x+1):
+                if (x % i)==0:
+                    d.append(i)
+        return d 
+    print('the numbers that can devide', n ,'is:',div(n))
+main()
