bbf5c72e30c715b5f9327604c5ae5c5078603be7
Ibrahim Omeysh <148053100+Ibrahim-Omeysh@users.noreply.github.com>
2023-10-26 13:45:10 +0300

    Delete ass1-ex3.py

--- ass1-ex3.py
+++
@@ -1,9 +0,0 @@
-def main():
-    n=input('please insert your words: ')
-    def reverseString(m):
-         x=""
-         for i in reversed (range(0,len(m))):
-            x=x+m[i]
-         return x
-    print(reverseString(n))
-main()
