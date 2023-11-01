d67fd1bf92fce608bcf56cb4da85c27f7ca9bab4
Ibrahim Omeysh <148053100+Ibrahim-Omeysh@users.noreply.github.com>
2023-11-01 17:07:21 +0200

    one step closer

--- assignment_03_ibrahim_omeysh/FSD_exercise.py
+++ assignment_03_ibrahim_omeysh/FSD_exercise.py
@@ -3,7 +3,7 @@ def printClass(dect):
     for key,value in dect.items():
         print ('Student Id:',key , ' ---information---:' , value)
 
-##### find key  #########
+##### find key to a specific value  #########
 def findKey(dict1,val):
     for key ,value in dict1.items():
         if val==value:
@@ -14,24 +14,114 @@ def findKey(dict1,val):
         else:
             return None
         
+###### find  information #######  
+ 
+def findInformation(dict1,info):
+    x=[]
+    for key , val in dict1.items():
+        x.append(dict1[key].get(info))
+    return x        
+        
 ######   calculate average ########
-def dectAverage(dict1,k):
-    scores=dict1[k]["scores"]
-    return
+
+def studentAverage(dict1):
+    list1=findInformation(dict1,"scores")
+    list2=[]
+    for i in range(len(list1)):
+        avg=sum(list1[i])/3
+        list2.append(avg)
+    return list2    
+
+
+####### youngest student ########
+
+def youngestStudent(dict1):
+    list1=findInformation(dict1,"age")
+    youngest=list1.index(min(list1))+1
+    return dict1[youngest]
+
+###### high score ########
+
+def highScore (dict1):
+    list1=studentAverage(dict1)
+    highestscore=list1.index(max(list1))+1
+    return dict1[highestscore]
+
+######## ADD student #########
+
+###### first insert name ######
+def insertName():
+    name=input("please insert the name of the student: ")
+    while not name.isalpha():
+        name=input("please insert a valide name")
+    return name
+
+###### second insert age ######
+def insertAge():
+    validnumber=False
+    while not validnumber:
+        age=input("please insert the age:")
+        if age.isdigit():
+            age=int(score)
+            if age <=60 and age>16:
+                validnumber=True
+                return age
+            else:
+                print("The Age is not between 16 and 60. Please try again.")
+        else:
+            print('the input is not an age, try again')
+     
     
 
+####### third insert scores #######
+def insertScores():
+    list1=[]
+    for i in range(3):
+        validnumber=False
+        while not validnumber:
+            score=input("please insert the score:")
+            if score.isdigit():
+                score=int(score)
+                if score <=100:
+                    list1.append(score)
+                    validnumber=True
+                else:
+                    print("The score is not between 0 and 100. Please try again.")
+            else:
+                print('the input is not a valid score, try again')
+    return list1
+
+########## now we add a student ########
+
+def addStudent(dict1):
+    name=insertName()
+    age=insertAge()
+    scores=insertScores()
+    dict1[int(len(dict1)+1)]={'name':name,'age':age ,"scores":scores}
+    return dict1
+############ ^^^^^FINISH ADD STUDENT STEPS^^^^^^#########
+
+######## remove student ########
+
+def removeStudent(dict1):
+    name =insertName()
+    studentremove=findKey(dict1,name)
+    dict1.pop(studentremove)
+    return dict1
+
+######## find common names student###############
+             
+
+   
     
-    
-
-
-
 def main():
-    classroom ={1:{"name" : "ibrahim" ,"age":"29",  "scores":(25,40,95)},
+    classroom ={1:{"name" : "ibrahim" ,"age":"29",  "scores":(25,75,95)},
                 2:{"name" : "georgio" ,"age":"20",  "scores":(65,70,95)},
                 3:{"name" : "ali"     ,"age":"25",  "scores":(25,45,65)},
                 4:{"name" : "ahmad"   ,"age":"23",  "scores":(30,40,20)},
-                5:{"name" : "ibrahim"    ,"age":"24",  "scores":(95,40,95)}
+                5:{"name" : "omar"    ,"age":"24",  "scores":(95,40,90)}
                }
-    print(dectAverage(classroom, findKey(classroom,"ibrahim")))
+    print(removeStudent(classroom))
+
     
 main()
--- assignment_03_ibrahim_omeysh/ts.py
+++ assignment_03_ibrahim_omeysh/ts.py
@@ -1,4 +1,2 @@
-for key ,item in dect.items():
-    for key2 ,item2 in item.items():
-        if item2['name']=='ibrahim':
-             
+x=input("h")
+print(x.isnumeric())
