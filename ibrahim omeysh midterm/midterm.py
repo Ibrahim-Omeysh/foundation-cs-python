fb4dfc4c18e2ab44a49721b8e52d2dbc8abc0e8e
Ibrahim Omeysh <148053100+Ibrahim-Omeysh@users.noreply.github.com>
2023-11-09 23:32:03 +0200

    second choice done

--- ibrahim omeysh midterm/midterm.py
+++ ibrahim omeysh midterm/midterm.py
@@ -1,4 +1,4 @@
-######-----task1------########## 
+######-----task1: open tab------########## 
    
     ###### First check input url ########
 
@@ -20,23 +20,59 @@ def checkTitle():
         title=input("please, insert title without special characters:")
     return title
     
-    ####### append titel and url ###########
+    ####### append title and url ###########
 
 def openTab(data,title,url):
     index=int(len(data)+1)
     data[index]={'Title':title,'Url':url}
     return data
 
+#########-------second task : close tab-------###########
+
+    ####### checking index #########
+def checkNumericIndex():
+    index=input("insert index:")
+    if index =="":
+        return index
+    while not index.isdigit():
+        index=input('please, insert a numeric index: ')
+    return index
+
+def checkIndex(data,index):
+    c=0
+    for key in data:
+        if key==int(index):
+            c=1
+    return c
+
+    ############# close tab ##########
+
+def closeTab(data,index):
+    if index =="":
+        data.popitem()
+    else:
+        c=checkIndex(data,index)
+        if c==1:
+             data.pop(int(index))
+        else:
+            print("index not found , please try again")
+            
+    print(data)
 
 
 
         
 def main():
-    open_tabs={}
+    open_tabs={1: {'Title': 'microsoft', 'Url': 'www.microsoft.com'},
+               2: {'Title': 'git', 'Url': 'www.github.com'},
+               3: {'Title': 'hacker', 'Url': 'www.hackerrank.com'}}
     title=checkTitle()
     url=checkUrl()
     open_tabs=openTab(open_tabs,title,url)
     print(open_tabs)
+    index=checkNumericIndex()
+
+    closeTab(open_tabs,index)
     
 
 main()
