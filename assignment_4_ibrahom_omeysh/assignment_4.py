6d3a6d7e68d9e99fb1c571de93750b8e821d5dd7
Ibrahim Omeysh <148053100+Ibrahim-Omeysh@users.noreply.github.com>
2023-11-22 02:16:57 +0200

    adding function

--- assignment_4_ibrahom_omeysh/assignment_4.py
+++ assignment_4_ibrahom_omeysh/assignment_4.py
@@ -1,8 +1,13 @@
+# https://stackoverflow.com/questions/1045344/how-do-you-create-an-incremental-id-in-a-python-class
+# https://www.geeksforgeeks.org/priority-queue-in-python/
+# https://www.studytonight.com/code/python/ds/priority-queue-in-python.php
+
 ############# class tasks #########
 class Task:
-
-    def __init__(self, task_id,task_description,task_priority):
-        self.__id=task_id
+    task_id=0
+    def __init__(self, task_description,task_priority):
+        Task.task_id+=1
+        self.__id=Task.task_id
         self.__description=task_description
         self.__priority=task_priority
         self.__completed=False
@@ -29,34 +34,37 @@ class Task:
     def getCompleted(self):
         return self.__completed
     
+    def printTask(self):
+        print(self.__id,self.__description,self.__priority)
+    
 ############################### class node #############################
 
 class Node:
-    def __init__(self,info,desc):
-        self.__info=info
+    def __init__(self,task:Task):
+        self.__task = task
         self.__next=None
-        self.__desc=desc
+    def getTask(self):
+        return self.__task
+    
 
 ############################ class queuetask #############################
 class queuetask:
     def __init__(self):
         self.__head=None
-        self.__tail=None
         self.__size=0
 
     def isEmpty(self):
         return self.__size==0
     
-    def enqueue(self,info):###### adding new task according to his priority##########
-        node_to_add=Node(info)
+    def enqueue(self,task:Task):###### adding new task according to his priority##########
+        node_to_add=Node(task)
 
         if self.__size==0:
             self.__head=node_to_add
-            self.__tail=node_to_add
             self.__size+=1
             print("task added successfully")
         else:
-            if (node_to_add.__info >self.__head.__info):###### the priority is bigger than the priority of first node ########
+            if (node_to_add.getTask().getPriority()> self.__head.getTask().getPriority()):###### the priority is bigger than the priority of first node ########
                 node_to_add.__next=self.__head
                 self.__head=node_to_add
                 self.__size+=1
@@ -64,7 +72,7 @@ class queuetask:
             else:               ########## if not we had to search for it ###############
                 current=self.__head
                 previous=current
-                while current!=None and current.__info>=node_to_add.__info:####### find the node that hade smaller priority ########
+                while ( current!=None and current.getTask().getPriority() >= node_to_add.getTask().getPriority() ):####### find the node that hade smaller priority ########
                     previous=current
                     current=current.__next
 
@@ -83,11 +91,36 @@ class queuetask:
             temp.__next=None
             self.__size-=1
             print("removed task successfully")
-            return temp.__info
+            return temp.__task
     
     def displayQueue(self):#### displaying tasks #####
         current=self.__head
-        while current.__next!=None:
-            print("task :" ,current.__desc," with priority:", current.__info,end="\n")
+        while (current.__next !=None):
+            print(current.getTask().printTask(),end="\n")
             current=current.__next
-        print("task :" ,current.__desc , " with priority:", current.__info ,end="\n")
+        print(current.getTask().printTask() ,end="\n")
+    
+############# class stack####################
+    
+# class Stack:
+#     def __init__(self):
+#         self.__head=None
+#         self.__size=0
+
+
+def main():
+    task1=Task("eating",3)
+    task2=Task("cleaning",7)
+    task3=Task("cleaning",9)
+    task4=Task("cleaning",1)
+    task1.printTask()
+    task2.printTask()
+    task3.printTask()
+    task4.printTask()
+    q1=queuetask()
+    q1.enqueue(task1)
+    q1.enqueue(task2)
+    q1.enqueue(task3)
+    q1.enqueue(task4)
+    q1.displayQueue()
+main()
