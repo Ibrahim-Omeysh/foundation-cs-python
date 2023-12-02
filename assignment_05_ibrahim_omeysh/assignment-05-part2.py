643f70b03d9fdc8166efb62707723e7b16e14951
Ibrahim Omeysh <148053100+Ibrahim-Omeysh@users.noreply.github.com>
2023-12-02 23:40:34 +0200

    add classes

---
+++ assignment_05_ibrahim_omeysh/assignment-05-part2.py
@@ -0,0 +1,75 @@
+class User:
+    def __init__(self,username):
+        self.username=username
+        self.friends=[]
+
+    def addFriend(self,friend):
+        if friend not in self.friends:
+            self.friends.append(friend)
+        else:
+            print(f"{self.username} and {friend.username} are already friends")
+
+    def getFriend(self):
+        return self.friends
+
+
+class Platform:
+
+    def __init__(self):
+        self.users={}
+    
+    def addUser(self, username):
+        if username in self.users:
+            print("User already exist")
+        else:
+            added_user=User(username)
+            self.users[username]=added_user
+            print("User added successfully")
+            return added_user
+
+
+    def removeUser(self,username):
+        if username in self.users:
+            deleted_user=self.users.pop(username)
+            for user1 in self.users.values():
+                if deleted_user in user1.friends:
+                    user1.friends.remove(deleted_user)
+            print(f"{deleted_user.username} deleted successfully")
+        else:
+            print("User does not exist")
+
+
+        
+    def addRelation(self, user1:User,user2:User):
+        if user1 in user2.friends or user2 in user1.friends :
+            print(f"{user1.username} and {user2.username} are friends already")
+        else:
+            user2.addFriend(user1)
+            user1.addFriend(user2)
+            print(f"{user1.username} and {user2.username} are frinds now")
+
+    def printFriend(self):
+        for user in self.users:
+            for friend in self.users[user].friends:
+                print(f"{self.users[user].username} and {self.users[friend].username} are friends")
+
+
+
+
+def main():
+    p=Platform()
+    u1=p.addUser("omar")
+    u2=p.addUser("ali")
+    u3=p.addUser("bob")
+    u4=p.addUser("md")
+    p.addUser("ali")
+    p.addRelation(u1,u2)
+    p.addRelation(u2,u3)
+    p.addRelation(u4,u3)
+    p.addRelation(u2,u1)
+    p.removeUser("omar")  
+    for i in u2.getFriend():
+        print(i.username)
+    #p.printFriend()   
+
+main()
